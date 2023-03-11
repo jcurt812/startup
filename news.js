551f04117b9b03d9af391f91f091b20d09f94e21
Joseph Curtis <joseph.curtis812@gmail.com>
2023-03-10 23:31:55 -0700

    Added Javascript for news page, and creating post

---
+++ news.js
@@ -0,0 +1,5 @@
+(function(){
+    var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+    i.src="https://cdn.curator.io/published/30f5880c-b304-48bf-b891-3cbc764794ba.js";
+    e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
---
+++ timeline.js
@@ -0,0 +1,81 @@
+class Post {
+    constructor(username, topic, body, timestamp) {
+        this.id = crypto.randomUUID();
+        this.username = username;
+        this.topic = topic;
+        this.body = body;
+        this.timestamp = timestamp;
+    }
+}
+
+class Timeline {
+    constructor(topic, postArray, govLevel) {
+        this.topic = topic;
+        this.postArray = postArray;
+        this.govLevel = govLevel;
+    }
+}
+
+function getLocalStorageItem(key) {
+    const item = localStorage.getItem(key);
+    try {
+        return JSON.parse(item);
+    } catch (e) {
+        return item;
+    }
+}
+
+function getPosts() {
+    getLocalStorageItem('postArray');
+}
+
+
+function createNewHTMLElement(elementType, elementID, elementClass) {
+    var newElement = document.createElement(elementType);
+    newElement.id = elementID;
+    newElement.className = elementClass;
+
+    return newElement;
+}
+
+function createNewHTMLElementWithContent(elementType, elementID, elementClass, elementContent) {
+    var newElement = document.createElement(elementType);
+    newElement.id = elementID;
+    newElement.className = elementClass;
+    newElement.innerHTML = elementContent;
+
+    return newElement;
+}
+
+function createPost() {
+    var username = document.getElementById('poster-username');
+    var topic = document.getElementById('post-topic');
+    var postBody = document.getElementById('newpostbody');
+    var date = Date.now();
+
+    var newPost = new Post(username.value, topic.value, postBody.value, date);
+    console.log(newPost);
+
+    var newPostElement = createNewHTMLElement("div", newPost.id, "card mb-3");
+    document.getElementById('timeline-section').appendChild(newPostElement);
+
+    var newPostHeader = createNewHTMLElementWithContent("div", (newPost.id + "post-author"), "card-header", newPost.username);
+    document.getElementById(newPost.id).appendChild(newPostHeader);
+
+    /*var newDeleteButton = createNewHTMLElementWithContent("button", (newPost.id + "delete-button"), "btn btn-danger", "Delete");
+    document.getElementById((newPost.id + "post-author")).appendChild(newDeleteButton);*/
+
+    var newPostBodyEl = createNewHTMLElement("div", (newPost.id + "post-body"), "card-body");
+    document.getElementById(newPost.id).appendChild(newPostBodyEl);
+
+    var newPostParagraph = createNewHTMLElementWithContent("p", (newPost.id + "post-paragraph"), "card-text", newPost.body);
+    document.getElementById((newPost.id) + "post-body").appendChild(newPostParagraph);
+
+    username.value = '';
+    topic.value = '';
+    postBody.value = '';
+}
+
+function deletePost() {
+
+}
